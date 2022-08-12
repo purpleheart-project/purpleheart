3c779f0a250852a8396cc3e56ae03ac931faecf6
tzhangm <tzhangm@trip.com>
2022-08-12 23:17:28 +0800

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import styled from "@emotion/styled";
-import {css} from "@emotion/react";
-import {Button} from "antd"
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { Button } from 'antd';
 const Msg = styled.div`
   background-color: skyblue;
-`
+`;
 function App() {
-    return <div css={css`background-color: rebeccapurple`}>
-        <Button>你好呀</Button>
+  return (
+    <div
+      css={css`
+        background-color: rebeccapurple;
+      `}
+    >
+      <Button>你好呀</Button>
     </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import 'antd/dist/antd.css';
 
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <App></App>,
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App></App>);
